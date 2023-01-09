e2f44e8697f8a06b99805a555cf4bc4d649a8e95
bujosa <davidbujosa@gmail.com>
2023-01-09 18:15:25 -0400

    refactor(widgets): move category into category path

---
+++ lib/presentation/widgets/category/category.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:poloris/shared/enum/category_enum.dart';
+import 'package:poloris/shared/providers/task_provider.dart';
+import 'package:poloris/shared/utils/category_map.dart';
+import 'package:provider/provider.dart';
+import 'package:poloris/shared/utils/capitalize.dart';
+
+class CategoryWidget extends StatelessWidget {
+  const CategoryWidget(this.category, {super.key});
+
+  final CategoryEnum category;
+
+  @override
+  Widget build(BuildContext context) {
+    final taskProvider = Provider.of<TaskProvider>(context);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3), // changes position of shadow
+          ),
+        ],
+        borderRadius: BorderRadius.circular(30),
+      ),
+      margin: const EdgeInsets.all(15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Text(categoryMapEmoji[category].toString(),
+                style: const TextStyle(fontSize: 60)),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(category.name.capitalize(),
+                  style: const TextStyle(fontSize: 30)),
+              Text('Tasks: ${taskProvider.countCategory(category)}',
+                  style: const TextStyle(fontSize: 20)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
