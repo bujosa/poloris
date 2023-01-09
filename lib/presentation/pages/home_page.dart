c250a403fe1955d36ce8741b5844a094697f821b
bujosa <davidbujosa@gmail.com>
2023-01-09 17:02:06 -0400

    refactor(pages): refactor home page

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:poloris/presentation/widgets/home/completed_tasks.dart';
+import 'package:poloris/presentation/widgets/home/global_tasks.dart';
 import 'package:provider/provider.dart';
 import '../../shared/providers/task_provider.dart';
 import '../widgets/home/active_tasks.dart';
@@ -33,78 +35,7 @@ class _HomePageState extends State<HomePage> {
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                        width: MediaQuery.of(context).size.width * 0.44,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black12,
-                              blurRadius: 10.0,
-                              spreadRadius: 5.0,
-                              offset: Offset(0.0, 0.0),
-                            )
-                          ],
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Icon(
-                              Icons.check_circle,
-                              color: Colors.green,
-                              size: 80,
-                            ),
-                            const Text(
-                              'Completed',
-                              style: TextStyle(fontSize: 25),
-                            ),
-                            Text(
-                              taskProvider.totalCompleted.toString(),
-                              style: const TextStyle(
-                                  fontSize: 30, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        )),
-                    Container(
-                        width: MediaQuery.of(context).size.width * 0.44,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black12,
-                              blurRadius: 10.0,
-                              spreadRadius: 5.0,
-                              offset: Offset(0.0, 0.0),
-                            )
-                          ],
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              '🌎',
-                              style: TextStyle(fontSize: 66),
-                            ),
-                            // const Icon(
-                            //   Icons.check_circle,
-                            //   color: Colors.green,
-                            //   size: 80,
-                            // ),
-                            const Text(
-                              'Global',
-                              style: TextStyle(fontSize: 25),
-                            ),
-                            Text(
-                              taskProvider.global.toString(),
-                              style: const TextStyle(
-                                  fontSize: 30, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        )),
-                  ],
+                  children: const [CompletedTaskWidget(), GlobalTaskWidget()],
                 ),
               ],
             ),
