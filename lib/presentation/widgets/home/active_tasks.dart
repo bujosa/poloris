55cba89394497e5a02be2ac551899b104387d9b1
bujosa <davidbujosa@gmail.com>
2023-01-09 17:02:48 -0400

    feat(widgets): add gesture detector in total active widget

--- lib/presentation/widgets/home/active_tasks.dart
+++ lib/presentation/widgets/home/active_tasks.dart
@@ -14,44 +14,49 @@ class _TotalActiveWidgetState extends State<TotalActiveWidget> {
   Widget build(BuildContext context) {
     final taskProvider = Provider.of<TaskProvider>(context);
 
-    return Container(
-        width: MediaQuery.of(context).size.width,
-        height: 100,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 10.0,
-              spreadRadius: 5.0,
-              offset: Offset(0.0, 0.0),
-            )
-          ],
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Total Active tasks',
-                  style: TextStyle(fontSize: 25),
-                ),
-                Text(
-                  taskProvider.myTasks.length.toString(),
-                  style: const TextStyle(
-                      fontSize: 30, fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-            const Icon(
-              Icons.timeline,
-              color: Colors.blue,
-              size: 80,
-            )
-          ],
-        ));
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/tasks');
+      },
+      child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10.0,
+                spreadRadius: 5.0,
+                offset: Offset(0.0, 0.0),
+              )
+            ],
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Total Active tasks',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  Text(
+                    taskProvider.myTasks.length.toString(),
+                    style: const TextStyle(
+                        fontSize: 30, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              const Icon(
+                Icons.timeline,
+                color: Colors.blue,
+                size: 80,
+              )
+            ],
+          )),
+    );
   }
 }
