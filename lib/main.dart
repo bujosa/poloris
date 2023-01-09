55bac637fceff9ca0526c07aeabae32e4dd4e517
bujosa <davidbujosa@gmail.com>
2023-01-09 14:21:47 -0400

    feat: init provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:poloris/presentation/pages/category_page.dart';
 import 'package:poloris/presentation/pages/home_page.dart';
 import 'package:poloris/presentation/pages/task_page.dart';
+import 'package:poloris/shared/providers/task_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,17 +15,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => TaskProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage(),
+        routes: {
+          '/home': (context) => const HomePage(),
+          '/categories': (context) => const CategoryPage(),
+          '/tasks': (context) => const TaskPage(),
+        },
       ),
-      home: const HomePage(),
-      routes: {
-        '/home': (context) => const HomePage(),
-        '/categories': (context) => const CategoryPage(),
-        '/tasks': (context) => const TaskPage(),
-      },
     );
   }
 }
