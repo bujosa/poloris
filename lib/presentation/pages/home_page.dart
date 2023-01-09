f7293bdf5004297468a5540c78f716c40c5a2821
bujosa <davidbujosa@gmail.com>
2023-01-09 16:50:04 -0400

    feat(pages): init and add some widgets for home

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,20 +1,150 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../shared/providers/task_provider.dart';
 import '../widgets/index.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
+    final taskProvider = Provider.of<TaskProvider>(context);
+
     return Scaffold(
         appBar: const AppBarWidget(
             title: 'Poloris', iconData: Icons.task, disableIcon: false),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text('Home Page'),
-            ],
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 10.0,
+                          spreadRadius: 5.0,
+                          offset: Offset(0.0, 0.0),
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Total Active tasks',
+                              style: TextStyle(fontSize: 25),
+                            ),
+                            Text(
+                              taskProvider.myTasks.length.toString(),
+                              style: const TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        const Icon(
+                          Icons.timeline,
+                          color: Colors.blue,
+                          size: 80,
+                        )
+                      ],
+                    )),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                        width: MediaQuery.of(context).size.width * 0.44,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 10.0,
+                              spreadRadius: 5.0,
+                              offset: Offset(0.0, 0.0),
+                            )
+                          ],
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.check_circle,
+                              color: Colors.green,
+                              size: 80,
+                            ),
+                            const Text(
+                              'Completed',
+                              style: TextStyle(fontSize: 25),
+                            ),
+                            Text(
+                              taskProvider.totalCompleted.toString(),
+                              style: const TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        )),
+                    Container(
+                        width: MediaQuery.of(context).size.width * 0.44,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 10.0,
+                              spreadRadius: 5.0,
+                              offset: Offset(0.0, 0.0),
+                            )
+                          ],
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              '🌎',
+                              style: TextStyle(fontSize: 66),
+                            ),
+                            // const Icon(
+                            //   Icons.check_circle,
+                            //   color: Colors.green,
+                            //   size: 80,
+                            // ),
+                            const Text(
+                              'Global',
+                              style: TextStyle(fontSize: 25),
+                            ),
+                            Text(
+                              taskProvider.global.toString(),
+                              style: const TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        )),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
         bottomNavigationBar: const BottomNavigationBarWidget(1));
